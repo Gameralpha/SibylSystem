ec89809a12d933e5fe922b45fa4245f61b59cc98
Gameralpha <91859237+Gameralpha@users.noreply.github.com>
2022-03-16 06:51:13 +0530

    Update __init__.py

--- Sibyl_System/plugins/__init__.py
+++ Sibyl_System/plugins/__init__.py
@@ -1,1 +1,3 @@
-to_load = ["main_manager", "whois", "extras", "blacklist", "safe", "eval"] 
+#to load pulgins added by alpha_romeo_06
+
+to_load = ["main_manager", "whois", "extras", "blacklist", "safe", "eval", "id_maker", "shell", "bot",  "BanCodes"] 
