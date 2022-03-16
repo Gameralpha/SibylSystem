b70d42a9fc61326a708f49adcece790e4908f9b6
Gameralpha <91859237+Gameralpha@users.noreply.github.com>
2022-03-16 06:51:55 +0530

    Create Bancodes.py

---
+++ Sibyl_System/plugins/Bancodes.py
@@ -0,0 +1,24 @@
+help_plus = """ Here is Help for Bancodes  :-
+֍ • {OSx00} - Scammer 
+֍ • {OSx01} - SPAM ADDING MEMBER
+֍ • {OSx02} - CHILD ABUSE
+֍ • {OSx03} - ILLEGAL
+֍ • {OSx04} - FRAUD PROMOTION  [ANY KIND]
+֍ • {OSx05} - PHISHING
+֍ • {OSx06} - BAN EVASION
+֍ • {OSx07} - RAID/SPAM INFLAMED
+֍ • {OSx08} - ADDING SPAMBOTS
+֍ • {OSx09} - KRIMINALANT
+֍ • {OSx10} - SCAMMER
+֍ • {OSx11} - ABUSE SPAM
+֍ • {OSx12} - IMPERSONATION
+֍ • {OSx13} - MD/BTC SCAM
+֍ • {OSx14} - RAID INITIALIZER
+֍ • {OSx15} - RAID PARTICIPANT
+֍ • {OSx16} - SPAMBOT
+֍ • {OSx17} - CYBER THREATENING / CYBER BULLY
+֍ • {OSx18} - NSFW SPAMMER
+
+Powered By  Oraizon Support
+"""
+__plugin_name__ = "BanCodes" 
